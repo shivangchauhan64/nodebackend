a19f45a2e4fa2a21858f97ce0a02e014a269cd6b
shivangchauhan64 <shivangchauhan64@gmail.com>
2025-03-20 16:17:00 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 4000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/login', (req,res) =>{
+    res.send('Login')
+})
+
+app.get('/register',(req,res)=>{
+    res.send('<h1>Register Page</h1>')
+})
+
+app.get('/home',(req,res)=>{
+    res.send("<h2>Home</h2>")
+})
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
